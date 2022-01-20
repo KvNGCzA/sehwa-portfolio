a028b65683c200ec498b55dbe488ee70caf038d3
Christopher Akanmu <christopher.akanmu@trilogy.com>
2022-01-20 12:49:59 +0100

    Add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+export const Header = () => {
+  return (
+    <header id="header">
+      <div className="logo">
+        <h1>A.A.Adesewa</h1>
+      </div>
+      <ul className="nav">
+        <li className="nav__item">Work</li>
+      </ul>
+    </header>
+  );
+};
